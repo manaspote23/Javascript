df7dfee486c520a6e91594e497427ad31c69c1d7
manaspote23 <manaspote00@gmail.com>
2025-06-27 06:26:34 +0000

    conatiner config done

---
+++ Javascripts/variables.js
@@ -0,0 +1,8 @@
+console.log("Manas");
+const accountId = 965436
+let accountEmail = 'mdgyudfkhi4uf'
+var accountPassword='htuh'
+// accountId = 655656 //not allowed to change const
+console.log(accountId);
+accountPassword='2122121'
+console.log(accountPassword);
